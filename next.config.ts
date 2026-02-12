b75e0e5a5cc066d85eefb8c7f6bdd6932bc06a85
shimonaka <shimonaka.itbrain@gmail.com>
2026-02-12 15:49:01 +0900

    fix: Add basePath for GitHub Pages support

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = "salon-karte-instagram";
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: basePath,
+  assetPrefix: assetPrefix,
 };
 
 export default nextConfig;
